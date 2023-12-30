c4045f05b1b9c5c8e9fe4ca07abb893982b3ffe1
savyobrenner <savyobrenner@gmail.com>
2023-12-30 22:03:27 +0200

    Initial Commit

---
+++ CineScope/CineScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineScopeApp.swift
+//  CineScope
+//
+//  Created by Savyo Brenner on 30/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CineScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CineScope/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CineScope
+//
+//  Created by Savyo Brenner on 30/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
